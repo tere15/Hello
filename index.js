c6434bd4e2d76bcbba337f7f7ec01b8a14fd428d
tere15 <kristiina.horneman@gmail.com>
2015-12-21 12:40:00 +0200

    Version 1.4

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 var express = require("express");  // ladataan express moduuli muuttujaan
 var app = express();    // luodaan serveri
 var path = require("path");
+var bodyParser = require("body-parser");
 
 
+app.use(bodyParser.urlencoded());
 app.use('/',express.static(path.join(__dirname, 'views')));
+
 
 
 app.listen(3000);   // käynnistetään serveri (kuuntele porttia 3000, voi käyttää tässä testissä portteja 3000 ->)
